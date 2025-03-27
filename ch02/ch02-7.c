4b12e87685673df0815964a620d34b5e3d9b29a7
cho yun kyeong <pl0509@naver.com>
2025-03-27 18:30:41 +0900

    과제

--- ch02/ch02-7.c
+++ ch02/ch02-7.c
@@ -1,8 +1,21 @@
+/* 파일명: ch02-7
+
+   * 내용: 커피 사이즈(S, T, G)와 주문 수량을 입력받아서 출력하는 프로그램을 작성하시오. 커피 사이즈는 S, T, G 세 가지 문자 중 하나로 입력받는다.
+
+   * 작성자: 조윤경
+
+   * 날짜: 2025.3.22
+
+   * 버전: v1.0
+
+   */
+
+
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
 
-int main()
+void solve()
 {
     char size;
     int num;
@@ -13,5 +26,11 @@ int main()
     printf("%c사이즈 %d잔을 주문합니다.\n", size, num);
 
 
+    return 0;
+}
+
+int main()
+{
+    solve();
     return 0;
 }
--- ch02/ch02-9.c
+++ ch02/ch02-9.c
@@ -1,8 +1,20 @@
+/* 파일명: ch02-1
+
+   * 내용: 16진수 정수를 입력받아 10진수로 출력하는 프로그램을 작성하시오.
+
+   * 작성자: 조윤경
+
+   * 날짜: 2025.3.22
+
+   * 버전: v1.0
+
+   */
+
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
 
-int main()
+void solve()
 {
 	int num;
 
@@ -11,5 +23,10 @@ int main()
 
 	printf("16진수 %x는 10진수로 %d입니다.\n", num, num);
 
+}
+
+int main()
+{
+	solve();
 	return 0;
 }
