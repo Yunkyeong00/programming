6fbf5c81cf8dd52750da1e2a5670fc950bbc271f
cho yun kyeong <pl0509@naver.com>
2025-04-24 22:52:21 +0900

    수정

--- ch05/ch05-3.c
+++ ch05/ch05-3.c
@@ -2,68 +2,54 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-
-
 int result(int a)
 {
-	int money50000;
-	int money10000;
-	int money5000;
-	int money1000;
-	int money100;
-	int money10;
+	printf("거스론돈 (10원미만 절삭): %d\n", a);
+	if (a >= 50000)
+	{
+		printf("50000원 \t%d\n", a / 50000);
+		a = a % 50000;
+	}
 
-	money50000 = a / 50000;
-	a = a % 50000;
+	if (a >= 10000)
+	{
+		printf("10000원 \t%d\n", a / 10000);
+		a = a % 10000;
 
-	money10000 = a / 10000;
-	a = a % 10000;
+	}
 
-	money5000 = a / 5000;
-	a = a % 5000;
+	if (a >= 5000)
+	{
+		printf("5000원 \t%d\n", a / 5000);
+		a = a % 5000;
+	}
 
-	money1000 = a / 1000;
-	a = a % 1000;
+	if (a >= 1000)
+	{
+		printf("1000원 \t%d\n", a / 1000);
+		a = a % 1000;
+	}
 
-	money100 = a / 100;
-	a = a % 100;
+	if (a >= 100)
+	{
+		printf("100원 \t%d\n", a / 100);
+		a = a % 100;
+	}
 
-	money10 = a / 10;
-	a = a % 10;
-
-		printf("50000원 %d장\n", money50000);
-	    printf("50000원 %d장\n", money10000);
-	    printf("50000원 %d장\n", money5000);
-	    printf("50000원 %d장\n", money1000);
-	    printf("50000원 %d장\n", money100);
-	    printf("50000원 %d장\n", money10);
-
-		return 0;
-
-}
-
-
-
-int test()
-{
-	int a = 0;
-
-
-	a -= a % 10;
-	
-	printf("거스름돈?");
-	scanf("%d", &a);
-
-	printf("거스름돈 (10원)미만 절사:%d\n",a);
-
-	result(a);
-	
-	return 0;
-
+	if (a >= 10);
+	{
+		printf("10원 \t%d\n", a / 10);
+	}
 }
 
 int main()
 {
-	test();
+	int money = 0;
+
+	printf("거스름돈? ");
+	scanf("%d", &money);
+
+	result(money);
+
 	return 0;
 }
