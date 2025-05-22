de58c8c9b3c037c69599379ed56f8658ce2db5ef
cho yun kyeong <pl0509@naver.com>
2025-05-23 02:45:34 +0900

    수정

--- ch06/ch06-18.c
+++ ch06/ch06-18.c
@@ -1,3 +1,18 @@
+/* 파일명: ch06-18.c
+
+ * 내용: 인자로 전달된 정수의 약수를 구해서 출력하는 divisors 함수를 작성하시오.
+   0~999사이의 임의의 정수 3개를 생성하고 divisors 함수를 이용해서 각각의 약수를 출력하는 프로그램을 작성하시오
+ 
+ * 작성자: 조윤경
+
+ * 날짜:  2025.05.22
+ * 
+ * 버전: v1.0
+
+ */
+
+
+
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
--- ch06/ch06-9.c
+++ ch06/ch06-9.c
@@ -1,3 +1,16 @@
+/* 파일명: ch06-9.c
+
+ * 내용: red, green, blue를 인자로 전달해서 RGB 색을 만들어 리턴하는 kae_rgb 함수를 정의하시오.
+ *       앞에서 정의한 get_red, get_green, get_blue 함수를 함께 이용해서 입력받은 RGB 색상의 보색을 구해서 출력하는 프로그램을 작성하시오.
+ * 
+ * 작성자: 조윤경
+
+ * 날짜: 2025.05.22
+
+ * 버전: v1.0
+
+ */
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
